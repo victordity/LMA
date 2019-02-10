66b6d71d605cfb6aa560b4f995147f01eaf9d6e2
Victor Hugo <victordity@gmail.com>
2019-02-10 18:05:55 -0200

    Add SideNav

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
+import { MatRadioModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material';
+import { MatDividerModule } from '@angular/material/divider';
+import { ScrollDispatchModule } from '@angular/cdk/scrolling';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './home/main-nav/main-nav.component';
+import { PresentationSectionComponent } from './home/presentation-section/presentation-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    PresentationSectionComponent,
   ],
   imports: [
-    BrowserModule
+    AlertModule.forRoot(),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatIconModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatListModule,
+    MatDividerModule,
+    MatCardModule,
+    ScrollDispatchModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/main/frontend/src/app/home/main-nav/main-nav.component.ts
@@ -0,0 +1,21 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import {ScrollDispatchModule} from '@angular/cdk/scrolling';
+
+@Component({
+  selector: 'app-main-nav',
+  templateUrl: './main-nav.component.html',
+  styleUrls: ['./main-nav.component.css']
+})
+export class MainNavComponent {
+  @ViewChild('sidenav') sidenav: MatSidenav;
+
+  reason = '';
+
+  close(reason: string) {
+    this.reason = reason;
+    this.sidenav.close();
+  }
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+}
