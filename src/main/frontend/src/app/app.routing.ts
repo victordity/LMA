f5fe17103d999517d75834492965fc9106d75d08
Victor Hugo <victordity@gmail.com>
2019-04-29 20:23:21 -0300

    Add routes

--- src/main/frontend/src/app/app.routing.ts
+++ src/main/frontend/src/app/app.routing.ts
@@ -1,12 +1,14 @@
 import { ProductsComponent } from './products/products.component';
 import { AppComponent } from './app.component';
+import { PresentationSectionComponent } from './home/presentation-section/presentation-section.component';
 
 import {Routes, RouterModule} from '@angular/router'
 import { ModuleWithProviders } from '@angular/core';
 
+
 const APP_ROUTES: Routes = [
-    { path: '', component: AppComponent },
-    { path: 'products', component: ProductsComponent }
+    { path: 'products', component: ProductsComponent },
+    { path: '', component: PresentationSectionComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
